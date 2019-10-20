fa23dafeb7769b018529423b6bc6dcf522471262
Nico Gauthier-Pin <nicolas@gauthier-pin.com>
2019-10-20 14:55:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tiny Wordpress Boilerplate theme
+No, really. This has (almost) the bare minimum for a working WordPress theme:
+
+- index.php
+- header.php
+- footer.php
+- style.css (empty for other than header)
+- functions.php (empty)
+
+Use as a starter for your WordPress project, and go crazy from here.
+
+## License
+MIT 
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header>
+<div>
+  <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
+  <p><?php bloginfo( 'description' ); ?></p>
+</div>
+<nav>
+<?php wp_nav_menu(); ?>
+</nav>
+</header>
